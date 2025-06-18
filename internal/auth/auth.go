3e59d404cd2173692450a23980777f2c92b4aeac
shredder42 <atbinder42@gmail.com>
2025-06-18 11:46:41 -0500

    tests on ci - functional

--- internal/auth/auth.go
+++ internal/auth/auth.go
@@ -3,7 +3,7 @@ package auth
 import (
 	"errors"
 	"net/http"
-	// "strings"
+	"strings"
 )
 
 var ErrNoAuthHeaderIncluded = errors.New("no authorization header included")
@@ -14,10 +14,10 @@ func GetAPIKey(headers http.Header) (string, error) {
 	if authHeader == "" {
 		return "", ErrNoAuthHeaderIncluded
 	}
-	// splitAuth := strings.Split(authHeader, " ")
-	if len(authHeader) < 2 || string(authHeader[0]) != "ApiKey" {
+	splitAuth := strings.Split(authHeader, " ")
+	if len(splitAuth) < 2 || splitAuth[0] != "ApiKey" {
 		return "", errors.New("malformed authorization header")
 	}
 
-	return authHeader, nil
+	return splitAuth[1], nil
 }
